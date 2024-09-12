e04e28df43dd87942ed8ca3f87de1df5008d0524
Cheese <12050603+mcheese@users.noreply.github.com>
2024-09-12 12:35:29 +0200

    bla

--- init.lua
+++ init.lua
@@ -180,10 +180,10 @@ vim.keymap.set('n', '<leader>i', ':tabprevious<CR>')
 vim.keymap.set('n', '<leader>o', ':tabnext<CR>')
 vim.keymap.set('n', '<ESC>', ':noh<CR><ESC>', { silent = true }) -- clear highlight on ESC
 -- telescope
-vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files<CR>")
-vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<CR>")
-vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<CR>")
-vim.keymap.set('n', '<leader>fh', "<cmd>Telescope help_tags<CR>")
+vim.keymap.set('n', '<C-p>', "<cmd>Telescope find_files<CR>")
+vim.keymap.set('n', '<C-g>', "<cmd>Telescope live_grep<CR>")
+--vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<CR>")
+--vim.keymap.set('n', '<leader>fh', "<cmd>Telescope help_tags<CR>")
 
 
 
